9d8e7c5aba2af6174920b17457adce90eb6853dd
anapdh <ana_d_hubner@hotmail.com>
2021-03-11 18:28:56 -0300

    add partial for sent and received friend requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,9 @@
 class FriendshipsController < ApplicationController
+  def index
+    @received_invites = Friendship.where(friend_id: current_user.id, confirmed: false)
+    @sent_invites = Friendship.where(user_id: current_user.id, confirmed: false)
+  end
+
   def create
     @friendship = Friendship.new(friendship_params)
     @friendship.user_id = current_user.id
@@ -22,9 +27,30 @@ class FriendshipsController < ApplicationController
     end
   end
 
+  # def update
+  #   # @friendship = Friendship.new
+  #   # @friendship.user_id = current_user.id
+  #   @friendship = Friendship.where(user_id: params[:user_id], friend_id: current_user, status: false)
+  #   @friendship2 = Friendship.where(user_id: current_user, friend_id: params[:user_id], status: false)
+
+
+  #   respond_to do |format|
+  #     if @friendship.update(friendship_params) == true
+  #       format.html do
+  #         redirect_to users_path, notice: 'You are now friends!'
+        
+  #       end
+  #       format.json { render :show, status: :created, location: @friendship }
+  #     else
+  #       format.html { render :show, status: :unprocessable_entity }
+  #       format.json { render json: @friendship.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
   private
 
   def friendship_params
-    params.require(:friendship).permit(:friend_id)
+    params.require(:friendship).permit(:friend_id, :confirmed)
   end
 end
