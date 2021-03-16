ef493d85b5dd3324a535cccd552ed94ffc462011
jpdf00 <j.pdf00@gmail.com>
2021-03-16 15:40:34 -0300

    Fix invite button

--- app/helpers/_user_helper.rb
+++
@@ -1,5 +0,0 @@
-module UserHelper
-  def rec_invites(user, invite)
-    render 'posts/received_invites', received_invites: invite if current_user.id == user.id
-  end
-end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,11 +1,11 @@
 module UserHelper
   def added_friend(user, friendship)
-    if !current_user.id == user.id && !Friendship.where(user_id: current_user.id,
-                                                        friend_id: user.id).or(Friendship.where(
-                                                                                 friend_id: current_user.id,
-                                                                                 user_id: user.id
-                                                                               )).exists?
+    if current_user.id != user.id && !Friendship.where(user_id: current_user.id, friend_id: user.id).or(Friendship.where(friend_id: current_user.id, user_id: user.id)).exists?
       render 'invite_button', user: user, friendship: friendship
     end
+  end
+
+  def rec_invites(user, invite)
+    render 'posts/received_invites', received_invites: invite if current_user.id == user.id
   end
 end
