e7c091f050238f0c7a74fbb4e01b426294096f9b
anapdh <ana_d_hubner@hotmail.com>
2021-03-19 12:23:52 -0300

    Fix Rubocop linter

--- app/helpers/sent_invites_helper.rb
+++ app/helpers/sent_invites_helper.rb
@@ -2,11 +2,11 @@ module SentInvitesHelper
   def sent_invites(sent_invites)
     out = ''
     sent_invites.each do |friendship|
-      unless current_user.confirmed_friend?(friendship.friend)
-        out += ' <span class="profile-link">'
-        out += link_to friendship.friend.name, user_path(friendship.user), class: 'profile-link'
-        out += '</span><br>'
-      end
+      next if current_user.confirmed_friend?(friendship.friend)
+
+      out += ' <span class="profile-link">'
+      out += link_to friendship.friend.name, user_path(friendship.user), class: 'profile-link'
+      out += '</span><br>'
     end
     out.html_safe
   end
